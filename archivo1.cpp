c79896c66cbccfed0bf5c0b306679004f56ca0ff
cristianescudero <cristian-escuder@utp.edu.co>
2020-10-28 18:52:32 -0500

    2 agregue informacion al archivo1

--- archivo1.cpp
+++ archivo1.cpp
@@ -6,4 +6,8 @@ int main()
 {
     cout<<"Hola mundo"<<endl;
     cout<<"Primer archivo para git"<<endl;
+    cout<<"Creado por cristian"<<endl;
+    cout<<"Para programación I"<<endl;
+    cout<<"Gr102"<<endl;
+    cout<<" :) "<<endl;
 }
